1d00bb523088f0e280bfa54e3a18c1fba1082c13
John Potter <johnpotter1440@gmail.com>
2021-02-04 17:30:34 -0800

    add prop inputPlaceholder to allow input to receive placeholder
    Seems counterintuitive to want to use a placeholder with floating label, but sometimes you want to display a placeholder on the input when it's in focus. This allows you to pass a placeholder to the input itself. You can then toggle whether to show or hide the placeholder based on whether it's in focus.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,17 @@
 import React, { HTMLProps } from "react";
 
 interface Props extends HTMLProps<HTMLInputElement | HTMLTextAreaElement> {
+  inputPlaceholder?: string;
   className?: string;
   component?: "input" | "textarea";
 }
 
-const FloatingLabel = ({ component = "input", placeholder, className = "", ...rest }: Props) => (
+const FloatingLabel = ({ component = "input", placeholder, inputPlaceholder, className = "", ...rest }: Props) => (
   <label
     className={`floating-label ${className} ${!!rest.value ? "floating" : ""}`}
     htmlFor={rest.id}
   >
-    {React.createElement(component, rest)}
+    {React.createElement(component, { ...rest, placeholder: inputPlaceholder })}
     <span>{placeholder}</span>
   </label>
 );
